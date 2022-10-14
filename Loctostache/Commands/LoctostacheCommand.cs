6098b2ba3658855e86c5bdb92978e53355476d34
Chad Roesler <ChadRoesler@Me.com>
2022-10-13 21:12:57 -0500

    proj updates and alterations to help text

--- Loctostache/Commands/LoctostacheCommand.cs
+++ Loctostache/Commands/LoctostacheCommand.cs
@@ -4,11 +4,11 @@ namespace Loctostache.Commands
 {
     internal class LoctostacheCommand
     {
-        [Option('d', "dictionary", HelpText = "The dictionary of to consume, as a json string object", Required = true)]
+        [Option('d', "dictionary", HelpText = "A json string dictionary of keys and their values", Required = true)]
         public string? Variables { get; set; }
-        [Option('t', "text", HelpText = "The text to replace", SetName = "text", Required = true)]
+        [Option('t', "text", HelpText = "Text to search and replace against", SetName = "text", Required = true)]
         public string? Text { get; set; }
-        [Option('f', "files", HelpText = "The path of a file to Repalce", Separator = ',', SetName = "file", Required = true)]
+        [Option('f', "files", HelpText = "A comma separated list of files read and replace text in", Separator = ',', SetName = "file", Required = true)]
         public IEnumerable<string>? Files { get; set; }
     }
 }
